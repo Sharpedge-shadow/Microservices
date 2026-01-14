bd1b7394d52f4c4d6194d8f95d81ff604471111a
Sharpedge-shadow <as0450139@gmail.com>
2026-01-15 04:19:08 +0530

    first microservice

---
+++ accounts/src/main/java/com/eazybank/accounts/entity/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.eazybank.accounts.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter @Setter @ToString
+public class BaseEntity {
+
+    @Column(updatable = false)
+    private LocalDateTime created_at;
+
+    @Column(updatable = false)
+    private String created_by;
+
+    @Column(insertable = false)
+    private String updated_by;
+
+    @Column(insertable = false)
+    private LocalDateTime updated_at;
+
+}
